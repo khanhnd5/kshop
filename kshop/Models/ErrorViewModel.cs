ab20d8343c597cf1824215e12682944ec85910b1
Nguyen Duy Khanh <khanhnd95.ptit@gmail.com>
2022-08-14 11:21:20 +0700

    init commit

---
+++ kshop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace kshop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
